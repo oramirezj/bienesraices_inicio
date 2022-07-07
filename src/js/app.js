6dd3d53154f56af93ae51b4488d9860266acb4d9
Omar Ramírez <lic.omar.r.j@gmail.com>
2022-07-07 17:22:06 -0500

    Proyecto terminado hasta dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,25 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 function darkMode() {
-  const botonDarkMode = document.querySelector('.dark-mode-boton')
+  const prefiereDarkMode = window.matchMedia('(prefers-color-scheme:dark)');
+
+  // console.log(prefiereDarkMode.matches);  Te dice si usas dark mode en tu sistema true o false
+
+  if (prefiereDarkMode) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+
+  prefiereDarkMode.addEventListener('change', function (){
+    if (prefiereDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  })
+
+  const botonDarkMode = document.querySelector('.dark-mode-boton');
 
   botonDarkMode.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode')
